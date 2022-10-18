4cddc12e82ce372a136d46368c3fd9f2a7e028fb
SUN_CHENG_YUAN <107871151+MrZach107@users.noreply.github.com>
2022-10-18 17:00:48 +0800

    Update git.py

--- git.py
+++ git.py
@@ -1,6 +1,13 @@
 #最常用git指令
 
-"""
+"""    
+
+加到本地端
+  git add .
+  git add (file)  
+  >> git add HW1.py
+  
+  
 "提交"檔案到本地  #單次提交到專案內 通常一個專案會提交很多次
   git commit -m "mmessage" 
   >> git commit -m "add HW1.py"
@@ -9,12 +16,6 @@
 "發布"檔案到遠端  #一次性發布
   git push (remote_URL) (branch)
   >> git push https://github.com/MrZach107/Python_lesson main
-
-    
-加到本地端
-  git add.
-  git add (file)  
-  >> git add HW1.py
 
   
 查詢遠端repository的URL位置
